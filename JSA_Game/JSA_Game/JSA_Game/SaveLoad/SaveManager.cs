4237d289ade24c7a85fa238d6318084126a72abb
Andrew <A.J.Greenman@gmail.com>
2014-10-27 12:27:48 -0400

    Added Save/Load logic.

---
+++ JSA_Game/JSA_Game/JSA_Game/SaveLoad/SaveManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Storage;
+
+namespace JSA_Game.SaveLoad
+{
+    static class SaveManager
+    {
+        public struct Data
+        {
+
+        }
+
+        public static void Save(String username, int level, Character[] characters)
+        {
+
+        }
+    }
+}
