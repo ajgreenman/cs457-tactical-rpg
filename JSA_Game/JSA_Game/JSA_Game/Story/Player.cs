e83eb5717671fe2b5428968014649b1eb8631148
Andrew <A.J.Greenman@gmail.com>
2014-10-30 19:59:32 -0400

    Created Story namespace and the Player class.

---
+++ JSA_Game/JSA_Game/JSA_Game/Story/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSA_Game.Story
+{
+    class Player
+    {
+        int currentLevel;
+        String username;
+        Character[] activeUnits = new Character[10];
+        Character[] barracks = new Character[100];
+
+        public Player(String username)
+        {
+            this.username = username;
+            currentLevel = 1;
+        }
+
+        public int CurrentLevel
+        {
+            get { return currentLevel; }
+            set { currentLevel = value; }
+        }
+
+        public String Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public Character[] ActiveUnits
+        {
+            get { return activeUnits; }
+            set { activeUnits = value; }
+        }
+
+        public Character[] Barracks
+        {
+            get { return barracks; }
+            set { barracks = value; }
+        }
+    }
+}
